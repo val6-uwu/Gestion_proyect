5986badaba7c5ffade35787a52ce7ac14ceb8c60
val6-uwu <volivares542@gmail.com>
2025-06-10 11:28:23 -0500

    .

---
+++ Vista/catalogo.php
@@ -0,0 +1,15 @@
+<?php include('../Vista/navbar.php'); ?>
+
+<div class="container-product">
+    <div class="container-img">
+        <img src="" alt="">
+    </div>
+    <div class="description-product">
+        <span class="nombre"></span>
+        <span class="marca"></span>
+        <span class="categoria"></span>
+    </div>
+    <div class="btn-ver-producto">
+        <button></button>
+    </div>
+</div>
